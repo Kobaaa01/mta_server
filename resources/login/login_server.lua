8b6ce3c1e35f3239036e78af66d62b9062eabfa8
kobaaa <kobaaa01@gmail.com>
2025-02-03 23:40:06 +0100

    global players

--- resources/login/login_server.lua
+++ resources/login/login_server.lua
@@ -1,7 +1,7 @@
 local db = dbConnect("mysql", "dbname=db_109517;host=sql.25.svpj.link;charset=utf8", "db_109517", "YODK7m8uXc0XWty8")
 
 -- Mapa, kluczem jest serial 
-local Players = {}
+Players = {}
 
 function hashPassword(password)
     return hash("sha256", password)
