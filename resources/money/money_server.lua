627f420fc7250befae1953f5b5add33d2c869bce
kobaaa <kobaaa01@gmail.com>
2025-02-03 20:41:23 +0100

    base on serial, not nickname

--- resources/money/money_server.lua
+++ resources/money/money_server.lua
@@ -1,13 +1,13 @@
 -- Połączenie z bazą danych
 local db = dbConnect("mysql", "dbname=db_109517;host=sql.25.svpj.link;charset=utf8", "db_109517", "YODK7m8uXc0XWty8")
 
--- Funkcja ładowania pieniędzy po zalogowaniu na podstawie nicku
+-- Funkcja ładowania pieniędzy po zalogowaniu na podstawie serialu
 function onPlayerJoinOrLogin()
     local player = source
-    local playerNickname = getPlayerName(player)  -- Pobranie nicku gracza
+    local playerSerial = getPlayerSerial(player)  -- Pobranie serialu gracza
     
     -- Pobranie pieniędzy z bazy na podstawie nicku
-    local query = dbQuery(db, "SELECT money_pocket FROM Users WHERE nickname = ?", playerNickname)
+    local query = dbQuery(db, "SELECT money_pocket FROM Users WHERE serial = ?", playerSerial)
     local result = dbPoll(query, -1)
 
     if result and result[1] then
@@ -28,7 +28,7 @@ addEventHandler("onPlayerJoin", root, onPlayerJoinOrLogin)  -- Dla graczy, któr
 function updatePlayerMoney(player, amount)
     local currentMoney = getElementData(player, "money_pocket") or 0
     local newMoney = currentMoney + amount
-    local playerNickname = getPlayerName(player)
+    local playerSerial = getPlayerSerial(player)
 
     if newMoney < 0 then
         outputChatBox("Nie masz wystarczająco pieniędzy!", player, 255, 0, 0)
@@ -36,7 +36,7 @@ function updatePlayerMoney(player, amount)
     end
 
     -- Aktualizacja w bazie danych
-    dbExec(db, "UPDATE Users SET money_pocket = ? WHERE nickname = ?", newMoney, playerNickname)
+    dbExec(db, "UPDATE Users SET money_pocket = ? WHERE serial = ?", newMoney, playerSerial)
 
     -- Aktualizacja na kliencie
     triggerClientEvent(player, "onReceiveMoney", player, newMoney)
