dafc1a938b581a35e146e59b9655bede995ac0a0
ponktacology <you@example.com>
2025-02-17 23:05:31 +0100

    tablist fix

--- resources/tablist/tablist_server.lua
+++ resources/tablist/tablist_server.lua
@@ -2,10 +2,13 @@ addEvent("requestPlayerData", true)
 addEventHandler("requestPlayerData", root, function()
     local playersData = {}
     for _, player in ipairs(getElementsByType("player")) do
+        local serial = getPlayerSerial(player)
+        player_data = getPlayerBySerial(serial)
+
         table.insert(playersData, {
-            id = getElementData(player, "playerid") or 0, -- Pobierz ID gracza
-            nickname = getPlayerName(player), -- Pobierz nick gracza
-            skin_id = getElementModel(player) -- Pobierz ID skina gracza
+            id = player_data.id, -- Pobierz ID gracza
+            nickname = player_data.nickname, -- Pobierz nick gracza
+            skin_id = player_data.skin_id -- Pobierz ID skina gracza
         })
     end
     triggerClientEvent(client, "updateClientPlayerData", client, playersData)
