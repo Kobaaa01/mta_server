17404a0bf769bdfcb0e238f3e7d02e509768a0ca
Patryk Popowicz <ptrykppowicz@gmail.com>
2025-02-21 18:24:50 +0100

    aircraft HUD base
    attitute indicator;
    speed tape scale test

--- resources/vehicle_hud/vehicle_hud.lua
+++ resources/vehicle_hud/vehicle_hud.lua
@@ -22,6 +22,27 @@ local max_speed = 0
 local handling = {}
 local speedPerLine = 10
 
+local vehicles = {
+    Airplanes = {592, 577, 511, 512, 593, 520, 553, 476, 519, 460, 513},
+    Helicopters = {548, 425, 417, 487, 488, 497, 563, 447, 469},
+    Boats = {472, 473, 493, 595, 484, 430, 453, 452, 446, 454},
+    Bikes = {581, 509, 481, 462, 521, 463, 510, 522, 461, 448, 468, 586},
+    Cars_2Door_Compact = {602, 496, 401, 518, 527, 589, 419, 587, 533, 526, 474, 545, 517, 410, 600, 436, 439, 549, 491},
+    Cars_4Door_Luxury = {445, 604, 507, 585, 466, 492, 546, 551, 516, 467, 426, 547, 405, 580, 409, 550, 566, 540, 421, 529},
+    Civil_Service = {485, 431, 438, 437, 574, 420, 525, 408, 552},
+    Government_Vehicles = {416, 433, 427, 490, 528, 407, 544, 523, 470, 596, 598, 599, 597, 432, 601, 428},
+    Heavy_Utility_Trucks = {499, 609, 498, 524, 532, 578, 486, 406, 573, 455, 588, 403, 423, 414, 443, 515, 514, 531, 456},
+    Light_Trucks_Vans = {459, 422, 482, 605, 530, 418, 572, 582, 413, 440, 543, 583, 478, 554},
+    SUVs_Wagons = {579, 400, 404, 489, 505, 479, 442, 458},
+    Lowriders = {536, 575, 534, 567, 535, 576, 412},
+    Muscle_Cars = {402, 542, 603, 475},
+    Street_Racers = {429, 541, 415, 480, 562, 565, 434, 494, 502, 503, 411, 559, 561, 560, 506, 451, 558, 555, 477},
+    RC_Vehicles = {441, 464, 594, 501, 465, 564},
+    Trailers = {606, 607, 610, 584, 611, 608, 435, 450, 591},
+    Trains_Railroad = {590, 538, 570, 569, 537, 449},
+    Recreational = {568, 424, 504, 457, 483, 508, 571, 500, 444, 556, 557, 471, 495, 539}
+}
+
 function get_vehicle_speed(vehicle)
     local dx, dy, dz = getElementVelocity(vehicle)
     return math.sqrt(dx^2 + dy^2 + dz^2) * 180
@@ -142,9 +163,82 @@ function draw_nos_level(vehicle)
     end
 end
 
+function get_vehicle_category(id)
+    for category, ids in pairs(vehicles) do
+        for _, vehicleID in ipairs(ids) do
+            if vehicleID == id then
+                return category
+            end
+        end
+    end
+    return nil
+end
+
+function is_vehicle_aircraft(category)
+    return category == "Airplanes" or category == "Helicopters"
+end
+
+local air_color = tocolor(98, 187, 213, 255)
+local ground_color = tocolor(154, 85, 17, 255)
+local attitude_indicator_target = dxCreateRenderTarget(hudW, hudW, true)
+local attitude_indicator_shader = dxCreateShader("attitude_indicator_shader.fx")
+dxSetShaderValue(attitude_indicator_shader, "gTexture", attitude_indicator_target)
+
+function draw_attitude_indicator(pitch, roll)
+    local pitch_y_offset = pitch
+    if pitch_y_offset > 180 then
+        pitch_y_offset = (pitch_y_offset - 360) * 3
+    end
+    
+    dxSetRenderTarget(attitude_indicator_target, true)
+
+    dxDrawCircle(hudW / 2, hudW / 2 + pitch_y_offset, 500, 180 - roll, 360 - roll, air_color, air_color, 64)
+    dxDrawCircle(hudW / 2, hudW / 2 + pitch_y_offset, 500, -roll, 180 - roll, ground_color, ground_color, 64)
+    local x2 = hudW / 2 + math.cos(math.rad(roll)) * 500
+    local y1 = hudW / 2 + pitch_y_offset + math.sin(math.rad(roll)) * 500
+    local x1 = hudW / 2 + math.cos(math.rad(roll + 180)) * 500
+    local y2 = hudW / 2 + pitch_y_offset + math.sin(math.rad(roll + 180)) * 500
+    dxDrawLine(x1, y1, x2, y2, tocolor(0, 0, 0, 255), 3)
+
+    dxSetRenderTarget()
+    dxDrawImage(screenW - hudW - padding + hudW * 0.0125, screenH - hudW - padding + hudW * 0.0125, hudW * 0.975, hudW * 0.975, attitude_indicator_shader, 0, 0, 0, tocolor(255, 255, 255, 255))
+
+end
+
+local speed_tape_width = 75
+local speed_tape_target = dxCreateRenderTarget(speed_tape_width, hudW, true)
+
+function draw_speed_tape(speed)
+    dxSetRenderTarget(speed_tape_target, true)
+
+    dxDrawRectangle(0, 0, speed_tape_width, hudW)
+
+    dxSetRenderTarget()
+
+    dxDrawImage(screenW - hudW - padding - speed_tape_width / 2, screenH - hudW - padding, speed_tape_width, hudW, speed_tape_target, 0, 0, 0, tocolor(0, 0, 0, 100))
+
+    -- FIXME 
+end
+
+function draw_aircraft_hud(vehicle)
+    local pitch, roll, yaw = getElementRotation(vehicle)
+    local speed = get_vehicle_speed(vehicle)
+
+    dxDrawCircle(screenW - hudW / 2 - padding, screenH - hudW / 2 - padding, hudW / 2, 0, 360, accent2, accent2, 64)
+
+    draw_attitude_indicator(pitch, roll)
+    draw_speed_tape(speed)
+    
+end
+
 function onClientRender()
     local vehicle = getPedOccupiedVehicle(localPlayer)
     if not vehicle then return end
+
+    if is_vehicle_aircraft(get_vehicle_category(getVehicleID(vehicle))) then
+        draw_aircraft_hud(vehicle)
+        return
+    end
 
     max_speed = math.floor((getVehicleHandling(vehicle)["maxVelocity"] + 0.5) / 10) * 10 + 30
     local currentSpeed = get_vehicle_speed(vehicle)
