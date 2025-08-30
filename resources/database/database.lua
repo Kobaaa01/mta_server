724a9f2ee780180fe10acdffbf527912aa551504
jakub <kobaaa01@gmail.com>
2025-08-30 23:38:42 +0200

    Update database connection
    Removed our test environment details

--- resources/database/database.lua
+++ resources/database/database.lua
@@ -1,4 +1,4 @@
-databse_connection = dbConnect("mysql", "dbname=db_109517;host=sql.25.svpj.link;charset=utf8", "db_109517", "YODK7m8uXc0XWty8")
+databse_connection = dbConnect("mysql", "testing_env", "testing_env", "testing_env")
 
 function get_db()
     return databse_connection
